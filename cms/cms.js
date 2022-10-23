bc28f107e425e1c3c800f83ea695e54ee9433050
Rena <rena.kuai@gmail.com>
2022-10-22 21:42:52 -0500

    Created build

--- cms/cms.js
+++ cms/cms.js
@@ -3,8 +3,6 @@ import React from 'react'
 import '../src/globalStyles.css'
 import data from '../src/data.json'
 import Home from '../src/views/Home'
-import About from '../src/views/About'
-import Contact from '../src/views/Contact'
 import Blog from '../src/views/Blog'
 import SinglePost from '../src/views/SinglePost'
 
@@ -26,12 +24,6 @@ const posts = getDocuments('posts')
 // Preview Templates
 CMS.registerPreviewTemplate('home-page', ({ entry }) => (
   <Home fields={entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('about-page', ({ entry }) => (
-  <About fields={entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
-  <Contact fields={entry.toJS().data} siteTitle={globalSettings.siteTitle} />
 ))
 CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
   <Blog fields={entry.toJS().data} posts={posts} />
